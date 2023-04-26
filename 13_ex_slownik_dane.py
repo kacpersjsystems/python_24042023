e426f1b9a92bc71b2104cf1f15b2a5d8b3e823ca
kacersjsystems <ksieradzinski@gmail.com>
2023-04-26 14:46:44 +0200

    count words

--- 13_ex_slownik_dane.py
+++ 13_ex_slownik_dane.py
@@ -1,3 +1,16 @@
-# Wczytaj do słownika dane z pliku dane.txt tak by kluczem było imię sklejone z nazwiskiem
+# Wczytaj do słownika dane z pliku 13_ex_slownik_dane.txt tak by kluczem było imię sklejone z nazwiskiem
 # rozdzielone spacja, a pozostałe dane znalazły się w wartości
 # jako krotka lub lista. Przeiteruj po slowniku i wyswietl jego zawartość.
+
+# { 'JanKowalski': { "wzost": 150, "waga": 49 }, "ZofiaNowak": [160,56] }
+
+people = {}
+with open('13_ex_slownik_dane', encoding='utf8') as file:
+    for line in file:
+        first_name, last_name, height, weight = line.strip().split(',')
+
+        people[first_name + last_name] = [weight, height]
+        #people[first_name + last_name] = {'weight': weight, 'height': height}
+
+print(people)
+# 14:44
---
+++ 14_ex_zliczaj_slowa.py
@@ -0,0 +1,4 @@
+# Napisz program, który otworzy plik o nazwie 14_ex_zliczaj_slowa.txt zawierający
+# tekst. Plik może zawierać dowolną ilość linii. Zadaniem programu jest zliczenie
+# Jak często w pliku występuje każdy z wyrazów
+# Posortuj słowa na podstawie ilości wystąpień
