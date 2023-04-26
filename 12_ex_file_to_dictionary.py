92e314aa6a93c9faea597621bc3f5da1aaddf922
kacersjsystems <ksieradzinski@gmail.com>
2023-04-26 14:18:23 +0200

    working on dictionaries

--- 12_ex_file_to_dictionary.py
+++ 12_ex_file_to_dictionary.py
@@ -3,5 +3,24 @@
 # timezone;-2
 # color;black
 # Następnie wczytaj dane do słownika w ten sposób by pierwsza kolumna stanowila klucze a
-# druga przypisane do nich
-# wartości. Przeiteruj po słowniku i wypisz klucze oraz przypisane do nich wartości
+# druga przypisane do nich wartości. Przeiteruj po słowniku i wypisz klucze oraz przypisane
+# do nich wartości
+#
+# { 'encoding': 'utf-8', 'timezone': '-2', 'color': 'black' }
+#
+# 1. Otwórz plik i wyświetl z niego wszystkie linie
+# 2. Rozbij linie po średniu
+# 3. Przypisz do osobnych zmiennych klucze, wartości (wewnątrz pętli)
+# 4. Zdefiniuj pusty słownik nad pętlą
+# 5. Wewnątrz pętli przypisz do słownika klucz i wartość o tak to_jest_slownik[to_jest_klucz]=to_jest_wartosc
+
+with open('12_ex_file_to_dictionary.txt', encoding='utf8') as file:
+    settings = {}
+    for line in file:
+        key, value = line.strip().split(';')
+        settings[key] = value
+
+    print(settings)
+
+for key, value in settings.items():
+    print(f'{key}: {value}')
---
+++ 13_ex_slownik_dane.py
@@ -0,0 +1,3 @@
+# Wczytaj do słownika dane z pliku dane.txt tak by kluczem było imię sklejone z nazwiskiem
+# rozdzielone spacja, a pozostałe dane znalazły się w wartości
+# jako krotka lub lista. Przeiteruj po slowniku i wyswietl jego zawartość.
