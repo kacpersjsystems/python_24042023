8374be7be3dc1e2209f80df5cf621f6bc2b2a258
kacersjsystems <ksieradzinski@gmail.com>
2023-04-26 09:09:18 +0200

    start day 2

---
+++ rozgrzewka.py
@@ -0,0 +1,34 @@
+# 0. Napisz program, który pobierze od użytkownika dowolny ciąg znaków i wypisze go na ekranie w odwrotnej kolejności
+# bez spacji, przecinka oraz kropki.
+#
+# Input: Ala ma kota.
+# Output: atokamalA
+#
+#
+# 1. Z podanej listy imion o różnej długości wyświetl tylko te które są dłuższe niż 5 znaków.
+# Długość imienia sprawdź funkcją len()
+#   * Zmień ten program do postaci listy składanej (list comprehension)
+#   * Za pomocą .title() upewnij się, że zawsze zaczynamy z wielkiej litery
+#
+#
+# 2. “12 kilogramów jabłek, 10 kilogramów gruszek, 20 kilogramów śliwek”
+# policzyć ile łącznie razem ważą zakupione produktu. (spacje przed kilogramami muszą być :)
+# a) split i rozbij wyrażenie po spacjach
+# b) iteruj po każdym wyrażeniu i dodawaj do siebie te, które wyglądają jak liczby .isnumeric()
+#
+#
+# 3. Napisz program, który odbierze od użytkownika liczbę całkowitą, a następnie wyświetli na ekranie wszystkie liczby
+# parzyste od 2 do tej liczby. Wyświetl wszystkie te liczby oddzielone przecinkami.
+# a) załaduj wszystkie te liczby do listy
+# b) połącz elementy listy przecinkami ','.join(lista)
+#
+#
+# 4. Napisz program, w którym odbierzesz od użytkownika imiona oraz nazwiska rozdzielone przecinkami,
+# w odpowiedzi powinny wyświetlić się poprawnie zapisane imiona bez powtórzeń posortowaną w kolejności Z-A.
+# IN: Adam Mickiewicz, Adam Asnyk, Zbigniew Nienacki
+# OUT: Zbigniew, Adam
+#
+# a) Rozbij imiona i nazwiska po przecinkach
+# b) Rozbij każde imie i nazwisko po spacji
+# c) Dodaj imię do nowej listy jeżeli jeszcze go tam nie było
+# d) Wyświetl odwrotnie posortowaną listę sorted(lista, reverse=True)
