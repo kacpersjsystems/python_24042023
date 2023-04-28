b7b6b2f31216c0ffc58ac0889905da487abbf885
kacersjsystems <ksieradzinski@gmail.com>
2023-04-28 09:54:44 +0200

    website progress

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
 application = Flask(__name__)
 
 
 @application.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html')
 
 
 @application.route('/hello/<first_name>')
 def hello(first_name):
-    return f'Dzień dobry! Cześć {first_name.title()}!'
+    #return f'Dzień dobry! Cześć {first_name.title()}!'
+
+    return render_template('hello.html', first_name=first_name)
+
+
+@application.route('/pogoda')
+def weather():
+    with requests.get('https://danepubliczne.imgw.pl/api/data/synop') as response:
+        return render_template('weather.html', stations=response.json())
